7756c178a2be7539ddd52fe34aa2ee1978beffcb
MagedTT <maged922001@gmail.com>
2026-05-03 21:54:16 +0300

    183.Customers_Who_Never_Order

---
+++ 183.Customers_Who_Never_Order.sql
@@ -0,0 +1,9 @@
+-- Problem Link: https://leetcode.com/problems/customers-who-never-order/description/
+
+
+SELECT
+    c.name AS Customers
+FROM Customers AS c
+LEFT JOIN Orders AS o
+ON c.id = o.customerId
+WHERE o.customerId IS NULL;
