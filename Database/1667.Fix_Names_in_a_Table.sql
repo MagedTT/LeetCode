b9ca0074247483c76bb003846ebf3ac883c3b3e6
MagedTT <maged922001@gmail.com>
2026-05-13 19:22:40 +0300

    1667.Fix_Names_in_a_Table

---
+++ Database/1667.Fix_Names_in_a_Table.sql
@@ -0,0 +1,7 @@
+-- Problem Link: https://leetcode.com/problems/fix-names-in-a-table/
+
+SELECT
+    user_id,
+    CONCAT(UPPER(LEFT(name, 1)), LOWER(RIGHT(name, LEN(name) - 1))) AS name
+FROM Users
+ORDER BY user_id;
