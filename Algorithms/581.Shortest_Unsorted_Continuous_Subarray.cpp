12fbf2004a908357d201bdb24fe04f40ef4e19b2
MagedTT <maged922001@gmail.com>
2026-07-21 18:39:13 +0300

    581.Shortest_Unsorted_Continuous_Subarray O(n) Solution

--- Algorithms/581.Shortest_Unsorted_Continuous_Subarray.cpp
+++ Algorithms/581.Shortest_Unsorted_Continuous_Subarray.cpp
@@ -5,6 +5,7 @@
 
 using namespace std;
 
+// O(n log n) solution
 class Solution
 {
 public:
@@ -39,6 +40,31 @@ public:
         return min_idx == -1 ? 0 : max_idx - min_idx + 1;
     }
 };
+// another O(n) Solution
+int findUnsortedSubarray(vector<int> &nums)
+{
+    int min_idx = -1, max_idx = -1, mn = -1, mx = -1, sz = (int)nums.size();
+
+    for (int i = 0; i < sz; ++i)
+    {
+        if (mx == -1 || mx < nums[i])
+            mx = nums[i];
+
+        if (mx > nums[i])
+            max_idx = i;
+    }
+
+    for (int i = sz - 1; i >= 0; --i)
+    {
+        if (mn == -1 || mn > nums[i])
+            mn = nums[i];
+
+        if (nums[i] > mn)
+            min_idx = i;
+    }
+
+    return min_idx == -1 ? 0 : max_idx - min_idx + 1;
+}
 
 int main()
 {
