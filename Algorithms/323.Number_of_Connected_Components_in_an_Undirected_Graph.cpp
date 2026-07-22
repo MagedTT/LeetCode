54fc5a9de4ab43cda944b91f36d6a1d1352ea69d
MagedTT <maged922001@gmail.com>
2026-07-22 20:45:56 +0300

    323.Number_of_Connected_Components_in_an_Undirected_Graph

---
+++ Algorithms/323.Number_of_Connected_Components_in_an_Undirected_Graph.cpp
@@ -0,0 +1,57 @@
+// Problem Link: https://leetcode.com/problems/number-of-connected-components-in-an-undirected-graph/description/
+
+#include <iostream>
+#include <unordered_map>
+#include <unordered_set>
+
+using namespace std;
+
+typedef unordered_map<int, unordered_set<int>> graph;
+
+void add_edge(graph &graph, int from, int to)
+{
+    graph[from].insert(to);
+}
+
+void dfs(graph &graph, int node, unordered_set<int> &visited)
+{
+    visited.insert(node);
+
+    for (auto &a : graph[node])
+    {
+        if (!visited.count(a))
+            dfs(graph, a, visited);
+    }
+}
+
+vector<int> killProcess(vector<int> &pid, vector<int> &ppid, int kill)
+{
+    graph graph;
+
+    int sz = (int)pid.size();
+    for (int i = 0; i < sz; ++i)
+        add_edge(graph, ppid[i], pid[i]);
+
+    unordered_set<int> visited;
+
+    dfs(graph, kill, visited);
+
+    return vector<int>(visited.begin(), visited.end());
+}
+
+int main()
+{
+    // Test-Case
+    vector<int> pid{5234, 2234, 6234, 3234, 123123, 1234, 4234};
+    vector<int> ppid{0, 5234, 2234, 2234, 5234, 5234, 1234};
+
+    int k;
+    cin >> k;
+
+    vector<int> result = killProcess(pid, ppid, k);
+
+    for (auto &a : result)
+        cout << a << ' ';
+    cout << '\n';
+    return 0;
+}
